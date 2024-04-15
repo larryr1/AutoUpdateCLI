d5f04e0007c06ac50865dfaef79d91211aeba4a2
Larry Rowe <larrywroweiii+git@gmail.com>
2024-04-15 11:04:29 -0400

    Created simple program for broadcasting AU UDP datagrams.

---
+++ AutoUpdate Simple Broadcaster/Program.cs
@@ -0,0 +1,75 @@
+﻿using System.ComponentModel;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace AutoUpdate_Simple_Broadcaster
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("AutoUpdate Simple Broadcaster");
+
+            IPAddress? address = null;
+            while (address == null)
+            {
+                Console.WriteLine("Enter the configuration server IP address.");
+                try
+                {
+                    address = IPAddress.Parse(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("That is not a valid IP address.");
+                    address = null;
+                }
+            }
+
+            Console.WriteLine("Using IP address " + address.ToString());
+
+            int port = -1;
+            while (port == -1)
+            {
+                Console.WriteLine("Enter the configuration server port.");
+                try
+                {
+                    port = int.Parse(Console.ReadLine());
+                    if (port < 1 || port > 65535) throw new ArgumentOutOfRangeException();
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("That is not a valid port number.");
+                    port = -1;
+                }
+            }
+
+            Console.WriteLine("Using port number " + port.ToString());
+
+            string dataString = $"AUADVERTISE,{address},{port}";
+
+            Console.WriteLine($"Broadcasting the following string over UDP on port 29463: \"{dataString}\"");
+            Console.WriteLine("Press CTRL+C at any time to stop.");
+
+            UdpClient client = new UdpClient();
+            client.Client.Bind(new IPEndPoint(IPAddress.Any, port));
+
+            byte[] payload = Encoding.UTF8.GetBytes(dataString);
+
+            Task task = Task.Run(() =>
+            {
+                while (true)
+                {
+                    client.Send(payload, payload.Length, "255.255.255.255", 29463);
+                    Console.WriteLine("Broadcasted.");
+                    Thread.Sleep(5000);
+                }
+            });
+
+            task.Wait();
+
+
+            Console.ReadLine();
+        }
+    }
+}
