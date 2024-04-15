97f05b8921b4bac6e5e0dfd19b20230efd0a94c6
Larry Rowe <26939860+larryr1@users.noreply.github.com>
2024-04-15 02:06:56 -0400

    Add files via upload

---
+++ AutoUpdate CLI Bootstrapper.ps1
@@ -0,0 +1,22 @@
+﻿Write-Host "AutoUpdate CLI Bootstrapper"
+Write-Host "Download or contribute: https://github.com/larryr1/AutoUpdateCLI/"
+
+$repo = "larryr1/AutoUpdateCLI"
+$file = "AutoUpdateCLI-alpha-1.exe"
+
+$releases = "https://api.github.com/repos/larryr1/AutoUpdateCLI/releases"
+
+Write-Host Finding latest release...
+$tag = (Invoke-WebRequest $releases | ConvertFrom-Json)[0].tag_name
+
+$download = "https://github.com/$repo/releases/download/$tag/$file"
+$name = $file.Split(".")[0]
+$file = "$name.exe"
+$dir = "$name-$tag"
+
+Write-Host Dowloading latest release...
+Invoke-WebRequest $download -Out $file
+
+Write-Host Running release...
+Start-Process .\$file
+
